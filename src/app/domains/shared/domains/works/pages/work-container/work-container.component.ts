99d93423c12bd672403b6ec0f0960f63f8e4f621
Martin Mendez <martinpercu@gmail.com>
2023-12-22 20:14:50 -0300

    starting

---
+++ src/app/domains/shared/domains/works/pages/work-container/work-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './work-container.component.html',
+  styleUrl: './work-container.component.css'
+})
+export class WorkContainerComponent {
+
+}
