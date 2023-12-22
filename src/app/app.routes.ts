90c39ab0ebb345083d17c80a929ebd7a78932bc4
Martin Mendez <martinpercu@gmail.com>
2023-12-22 20:22:06 -0300

    tsconfig for imports + work and works container

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent
+  },
+  {
+    path: 'work',
+    component: AboutComponent
+  }
+];
