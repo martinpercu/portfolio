6596d204777dc504add1278319b5032a7931ca56
martinpercu <97993590+martinpercu@users.noreply.github.com>
2024-07-18 18:47:34 -0400

    sigue el baile

--- src/app/domains/services/works.service.ts
+++ src/app/domains/services/works.service.ts
@@ -29,8 +29,8 @@ export class WorksService {
         description: 'Kohue is an invitation and follow-up system for potential clients of the brand. A single product is sold at a specific time of the year. For that specific moment, previously selected clients are invited through emails with a link to the purchase page. The email service is managed via emailJS. The payment platform is STRIPE which has a server built in NodeJS. The whole project uses Firebase for Authentication, Hosting and Database.',
         problem: 'At the beginning of the year, Tupungato Wines Co was created in Napa Valley. Unfortunately they did not have any visual content for the brand, only the label on the Kohue wine bottle. They urgently needed a platform to prospect and engage future customers before the launch of their Kohue wine. And by October 2024, be ready to make sales and shipping.',
         solution: 'I decide to split the work into 2 phases. In the first phase I create a "temporary" brand image from the bottle label to use as soon as possible on the platform. I created the email, authentication and database services for client prospecting. I have prepared admin users to manage this database. In the second phase I deploy the server with NodeJS to connect to the Stripe API and be ready to receive payments in October. When I receive the definitive brand image I apply it to the platform.',
-        year: 2023,
-        image: '/assets/img/works/maximo-1.jpg',
+        year: 2024,
+        image: '/assets/img/works/kohue-phase-1.jpg',
         techs: [this.techs[0], this.techs[10], this.techs[6], this.techs[35], this.techs[24], this.techs[37]],
         techs2nd: [this.techs[2], this.techs[3], this.techs[5], this.techs[14]],
         frontend: true,
@@ -41,7 +41,7 @@ export class WorksService {
       {
         id: 'agro',
         title: 'AgroFull',
-        description: 'Agrofull allows users to preview expenses and costs before defining their agricultural actions',
+        description: 'Agrofull is a system that allows its users to visualize expenses, costs and benefits before defining their agricultural actions. The user builds his/her sowing plan by adding and selecting the fertilizers, additives and phytosanitary seeds that exist with their respective values.',
         problem: 'My client had problems with his development team. They were not able to understand each other regarding the development of the project. Another weakness was that the developers did not have anyone in-house for design and UX/UI areas.',
         // solution: 'SOLUTION  d AGROFULL  \n \n SOLUTION work qsfqkljh qsdljksdfqklsd dsqs qsdhqs qdklsjjh qds  all my love work qsfqkljh qsdljksdfqklsd dsqs qsdhqs qdklsjjh qds  all my lovejkldshfjk',
         // solution: 'First was understanding in depth what the client wanted to convey. Then I contacted the work team to analyze their capabilities and shortcomings. With the information obtained: I created a doc with all the requirements for the project and so that it could be understood I made a presentation in figma showing an almost final version of the app. For the design I coordinated the work with an external designer. It gives us a simple logo and a color palette. I transformed the app into one focused on mobile (mobile-first approach) mostly using just CSS. I built the website (agrofull.net) with direct link to the app. Then I give the app final details to minimize friction between the site and the app. (the business model forces the user to first get to the website and then go to the app).',
@@ -59,15 +59,15 @@ export class WorksService {
         id: 'iermito',
         title: 'QR system Victor Iermito Luthier',
         description: 'This QR system allows Victor Iermito (the client) to run a CRUD with the information about each of the guitars on display. Each QR is a direct link with the corresponding guitar parameter. The system is designed to operate with a low internet connection because the guitars are usually displayed in very isolated rooms.',
-        problem: 'PROBLEM VICTOR IERMITO PROBLEMqskldfhqklsdh work qsfqkljh qsdljksdfqklsd dsqs qsdhqs qdklsjjh qds  all my love work qsfqkljh qsdljksdfqklsd dsqs qsdhqs qdklsjjh qds  all my love work qsfqkljh qsdljksdfqklsd dsqs qsdhqs qdklsjjh qds  all my love f',
-        solution: 'SOLUTION  \n VICTOR <br /> IERMITO  work qsfqkljh qsdljksdfqklsd dsqs qsdhqs qdklsjjh qds  all my love work qsfqkljh qsdljksdfqklsd dsqs qsdhqs qdklsjjh qds  all my lovejkldshfjk',
+        problem: 'The client already had a QR system which were all different static html pages for each of the guitars. This forced him to have to call the developer every time he built a new guitar, with the cost that this entailed, to create a new HTML file and upload it to the site.',
+        solution: 'I built a simple CRUD so that the client can upload each guitar to the DB and use their ID as the parameter passed with the link. This way, he can generate the QRs himself without third party intervention. As we have low connectivity in the showrooms, to improve the UX, I added a spinner with the same logo that the buyer is seeing until the guitar data is loaded.',
         year: 2022,
         image: '/assets/img/works/iermito.jpg',
         techs: [this.techs[0], this.techs[35], this.techs[24]],
         techs2nd: [this.techs[2], this.techs[3], this.techs[5]],
         frontend: true,
         backend: true,
-        githubUrl: 'https://github.com/martinpercu',
+        githubUrl: 'https://github.com/martinpercu/guitar-iermito',
         youtubeLink: 'https://youtube.com'
       },
     ]
@@ -78,8 +78,3 @@ export class WorksService {
   }
 }
 
-
-// Kohue is an invitation and follow-up system for potential clients of the brand. A single product is sold at a specific time of the year.
-// For that specific moment, previously selected clients are invited through emails with a link to the purchase page.
-// The email service is managed via emailJS. The payment platform is STRIPE which has a server built in NodeJS.
-// The whole project uses Firebase for Authentication Hosting and Database.
